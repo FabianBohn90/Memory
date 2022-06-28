d989537bf84c6c4e3d2084de6cd67d4c3258ea74
fabianbohn <Mystek.fb@googlemail.com>
2022-06-28 16:19:43 +0200

    almoste done

--- app/src/main/java/com/example/memory/MemoryLogic.kt
+++ app/src/main/java/com/example/memory/MemoryLogic.kt
@@ -3,7 +3,6 @@ package com.example.memory
 import android.view.View
 import android.widget.ImageButton
 import kotlinx.coroutines.delay
-import kotlin.random.Random
 
 /**
  * Die Klasse MemoryLogic enthält eine Sammlung von Funktionen, welche die Kernlogik für das
@@ -19,7 +18,9 @@ class MemoryLogic {
 
         // gehe alle Karten durch und verdecke verdecke jede Karte
         // todo Schreibe hier deinen Code
-
+        for (i in 0..11) run {
+            hideCard(cards[i])
+        }
     }
 
     /**
@@ -27,19 +28,18 @@ class MemoryLogic {
      *  übergebenen Lambdas (= Pfeilfunktion) eine Karte auf jedem Feld
      */
     suspend fun distributeCards(columns: Int, rows: Int, placeCards: (Int, Int) -> Unit) {
-
         // gehe alle Spalten durch
-        // todo Schreibe hier deinen Code
-
         // gehe alle Zeilen durch
-        // todo Schreibe hier deinen Code
-
         // platziere Karte in Spalte, Zeile
-        // todo Schreibe hier deinen Code
-
-        // verzögere Ablauf um 50ms
-        // todo Schreibe hier deinen Code
-
+        for (i in 0..3) {
+            placeCards(columns, rows)
+            delay(50)
+            for (e in 0..5) {
+                placeCards(columns, rows)
+                // verzögere Ablauf um 50ms
+                delay(50)
+            }
+        }
     }
 
     // jetzt sollten die Karten auf dem Spielfeld erscheinen
@@ -49,13 +49,16 @@ class MemoryLogic {
      */
     fun cardClicked(lastIndex: Int, thisIndex: Int, setImage: (Int) -> Unit, compareImages: (Int, Int) -> Unit) {
 
+        if (thisIndex != lastIndex) {
+            compareImages(thisIndex, lastIndex)
+            setImage(thisIndex)
+        }
+
         // stelle sicher, dass es sich nicht um die gleiche Karte handelt
         // todo Schreibe hier deinen Code
-
         // setze das Bild
         // vergleiche die Bilder
         // todo Schreibe hier deinen Code
-
     }
 
     /**
@@ -68,8 +71,22 @@ class MemoryLogic {
         // Die neue Liste
         val cardImageIDs = mutableListOf<Int>()
 
-        // todo Schreibe hier deinen Code
 
+        // todo Schreibe hier deinen Code
+        while (cardImageIDs.size < 23) {
+            cardImageIDs.add(0, imageSource[0])
+            cardImageIDs.add(1, imageSource[1])
+            cardImageIDs.add(2, imageSource[2])
+            cardImageIDs.add(3, imageSource[3])
+            cardImageIDs.add(4, imageSource[4])
+            cardImageIDs.add(5, imageSource[5])
+            cardImageIDs.add(6, imageSource[6])
+            cardImageIDs.add(7, imageSource[7])
+            cardImageIDs.add(8, imageSource[8])
+            cardImageIDs.add(9, imageSource[9])
+            cardImageIDs.add(10, imageSource[10])
+            cardImageIDs.add(11, imageSource[11])
+        }
         // liefere die befüllte Liste zurück
         return cardImageIDs
     }
@@ -85,9 +102,11 @@ class MemoryLogic {
 
         // teste ob beide Bilder gleich sind
         // todo Schreibe hier deinen Code
-
+        if (currentImage == lastImage) {
+            makeBothCardsInvisible()
+            delay(50)
+        }
     }
-
     /**
      * Diese Funktion prüft alle Karten in der übergebenen Liste auf Ihre Sichtbarkeit (visibility)
      * Liefert true zurück, falls es noch sichtbare Karten gibt
@@ -95,7 +114,11 @@ class MemoryLogic {
     fun checkIfFoundAll(cards: List<ImageButton>): Boolean {
 
         // todo Schreibe hier deinen Code
-
-        return false
+        for (i in cards) {
+            if (cards.indexOf(i) == View.VISIBLE) {
+                return false
+            }
+        }
+        return true
     }
 }
